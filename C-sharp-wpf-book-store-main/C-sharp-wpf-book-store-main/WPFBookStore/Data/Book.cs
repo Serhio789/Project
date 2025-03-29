6ceb6d9ba74cfeb65181c164260030a593dd571d
Serhio789 <128630593+Serhio789@users.noreply.github.com>
2025-03-29 20:55:21 +0300

    Data
    Added a class that takes a list of books with filtering.

---
+++ C-sharp-wpf-book-store-main/C-sharp-wpf-book-store-main/WPFBookStore/Data/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFBookStore.Data
+{
+    public class Book
+    {
+        public string Title { get; set; }        // название
+        public int Yers { get; set; }            // год издания
+        public string Identifier { get; set; }   // индефикатор
+        public string Authors { get; set; }       // авторы
+        public string[] Translators { get; set; } // переводчики
+        public string Series { get; set; }        // серия
+        public string Language { get; set; }      // язык
+        public string Description { get; set; }   // описание
+        public string Genre { get; set; }         // жанр
+        public string ISBN { get; set; }          // isbn
+        public string Cover { get; set; }         // обложка
+    }
+
+}
---
+++ C-sharp-wpf-book-store-main/C-sharp-wpf-book-store-main/WPFBookStore/Data/Data.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace WPFBookStore.Data
+{
+    public class BookService
+    {
+        private readonly HttpClient _httpClient;
+
+        public BookService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<List<Book>> GetBooksAsync(
+            string title = null,
+            int? genre = null,
+            int? year = null,
+            string author = null)
+        {
+            // Формируем базовый URL
+            string baseUrl = "http://185.9.72.1:7778/api/v1/book/list";
+
+            // Формируем параметры запроса
+            var parameters = new List<string>();
+            if (!string.IsNullOrEmpty(title))
+                parameters.Add($"query={Uri.EscapeDataString(title)}");
+            if (year.HasValue)
+                parameters.Add($"genre={genre.Value}");
+            if (year.HasValue)
+                parameters.Add($"year={year.Value}");
+            if (!string.IsNullOrEmpty(author))
+                parameters.Add($"author={Uri.EscapeDataString(author)}");
+
+            // Соединяем параметры в строку запроса
+            string queryString = string.Join("&", parameters);
+            string requestUrl = string.IsNullOrEmpty(queryString) ? baseUrl : $"{baseUrl}?{queryString}";
+
+            Console.WriteLine(requestUrl);
+            // Отправляем запрос и получаем ответ
+            var response = await _httpClient.GetAsync(requestUrl);
+            response.EnsureSuccessStatusCode();
+
+            // Читаем содержимое ответа
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+
+            // Десериализуем JSON в список книг
+            var books = JsonConvert.DeserializeObject<List<Book>>(jsonResponse);
+            return books;
+        }
+    }
+}
